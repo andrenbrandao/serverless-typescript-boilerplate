0a2cd92c0b0356f514ae61f12a73f46c59ed0184
André Brandão <andrenbrandao@gmail.com>
2021-01-15 10:59:26 -0300

    chore: create serverless boilerplate

---
+++ serverless.ts
@@ -0,0 +1,30 @@
+import type { AWS } from '@serverless/typescript';
+
+import { hello } from './src/functions';
+
+const serverlessConfiguration: AWS = {
+  service: 'sls-api',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: { hello }
+}
+
+module.exports = serverlessConfiguration;
